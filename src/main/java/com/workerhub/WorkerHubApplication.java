7c2aa7228e24e0cbc3d24c0c821078857bc03533
Luan Pinheiro da Silva <lluanps@gmail.com>
2024-09-09 18:17:15 -0300

    Configuração inicial

---
+++ src/main/java/com/workerhub/WorkerHubApplication.java
@@ -0,0 +1,13 @@
+package com.workerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workerhub/WorkerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
